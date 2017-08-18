6612c4bd235f0850e9c7a07f470fec9b6828d7c9
VDSMath <matheusvdsdf@gmail.com>
2017-08-18 19:00:09 -0300

    Transição Entre as Cenas, SO Lido na Cena Principal

--- Assets/Code/GirlAtributes.cs
+++ Assets/Code/GirlAtributes.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GirlAtributes : MonoBehaviour {
 
@@ -17,8 +18,39 @@ public class GirlAtributes : MonoBehaviour {
 
 	[Header("Panel")]
 	[SerializeField] private GameObject _atributePanel;
-	
-	public void LoseEnergy(){
+
+    [Header("Current SO")]
+    [SerializeField]
+    private OutfitObject current;
+
+    [Header("Images")]
+    [SerializeField]
+    private Image hairBackImage;
+    [SerializeField]
+    private Image torsoImage;
+    [SerializeField]
+    private Image faceImage;
+    [SerializeField]
+    private Image hairImage;
+    [SerializeField]
+    private Image eyesImage;
+    [SerializeField]
+    private Image mouthImage;
+    [SerializeField]
+    private Image noseImage;
+
+    private void Start()
+    {
+        hairBackImage.sprite = current.hairBackSprite;
+        torsoImage.sprite = current.torsoSprite;
+        faceImage.sprite = current.faceSprite;
+        hairImage.sprite = current.hairSprite;
+        eyesImage.sprite = current.eyesSprite;
+        mouthImage.sprite = current.mouthSprite;
+        noseImage.sprite = current.noseSprite;
+    }
+
+    public void LoseEnergy(){
 		_energy -= _energyLoseTax;
 	}
 	private void LoseHumor(){
