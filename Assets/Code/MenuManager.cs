1b33704d0b8f5877f296fa3fbfcd571ba2234069
Pedro Nascimento <pedrovitor.marques@gmail.com>
2017-08-17 21:15:08 -0300

    Comodos da Casa

---
+++ Assets/Code/MenuManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	[SerializeField] private List<SOHousePart> _houseParts;
+	private int _actualHousePart;
+
+	private Color _color;
+	private string _headerTitle;
+
+	private Image _background;
+	private Text _header;
+	
+	private GameObject _uiObjPrefab;
+
+	private void Start(){
+		_background = GameObject.Find("BACKGROUND").GetComponent<Image>();
+		_header = GameObject.Find("TITLE").GetComponent<Text>();
+
+		NewHousePart(_actualHousePart);
+	}
+	private void NewHousePart(int i){
+		_color = _houseParts[i].backgroundColor;
+		_headerTitle = _houseParts[i].headerTitle;
+		SetStagedHousePartProperties();
+	}
+	private void SetStagedHousePartProperties(){
+		_background.color = _color;
+		_header.text = _headerTitle;
+		_uiObjPrefab = Instantiate(_houseParts[_actualHousePart].uiObjectsPrefab,Vector3.zero,Quaternion.identity,transform);
+		_uiObjPrefab.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+	}
+	public void ChangeToRight(){
+		Destroy(_uiObjPrefab.gameObject);
+		_actualHousePart++;
+		if(_actualHousePart >= _houseParts.Count){
+			_actualHousePart = 0;
+		}
+		NewHousePart(_actualHousePart);
+	}
+	public void ChangeToLeft(){
+		Destroy(_uiObjPrefab.gameObject);
+		_actualHousePart--;
+		if(_actualHousePart < 0){
+			_actualHousePart = _houseParts.Count - 1;
+		}
+		NewHousePart(_actualHousePart);
+	}
+}
---
+++ Assets/Code/SOHousePart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/HousePart")]
+public class SOHousePart : ScriptableObject {
+	public Color backgroundColor;
+	public string headerTitle;
+
+	public GameObject uiObjectsPrefab;
+
+}
